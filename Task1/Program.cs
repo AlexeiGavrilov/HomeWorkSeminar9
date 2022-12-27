bc5cac581aa2340721fe672a71b8e4aa05def265
Alex <kccnbnb@mail.ru>
2022-12-27 20:48:36 +0300

    add task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+int ConvertToIntUserMsg(string mesage)
+{
+    Console.Write(mesage);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int AllNaturalNumbers(int num)
+{
+    {
+        if (num == 1) return 1;
+        Console.Write($"{num} ");
+        return AllNaturalNumbers(num - 1);
+    }
+}
+int UserNum = ConvertToIntUserMsg("Введите число: ");
+Console.Write(AllNaturalNumbers(UserNum));
