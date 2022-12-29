e878619b9a8766bb051553ba0dc2f71be223cd95
Alex <kccnbnb@mail.ru>
2022-12-29 17:22:54 +0300

    add task4

---
+++ Task4/Program.cs
@@ -0,0 +1,44 @@
+﻿/*Напишите программу, которая заполнит спирально массив 4 на 4.
+Например, на выходе получается вот такой массив:
+01 02 03 04
+12 13 14 05
+11 16 15 06
+10 09 08 07
+*/
+void PrintArray2D(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (arr[i, j] / 10 <= 0)
+                Console.Write($" {arr[i, j]} ");
+
+            else Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+int[,] array = new int[6, 6];
+
+int num = 1;
+int i = 0;
+int j = 0;
+
+while (num <= array.GetLength(0) * array.GetLength(1))
+{
+    array[i, j] = num;
+    num++;
+    if (i <= j + 1 && i + j < array.GetLength(1) - 1)
+        j++;
+    else if (i < j && i + j >= array.GetLength(0) - 1)
+        i++;
+    else if (i >= j && i + j > array.GetLength(1) - 1)
+        j--;
+    else
+        i--;
+}
+
+PrintArray2D(array);
